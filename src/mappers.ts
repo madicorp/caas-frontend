1bef959bf820a7934f08e9eb6244825c15b41202
Sega Diop <sega.diop@madi-corp.net>
2024-04-06 07:24:23 +0200

    fix retrieve url

--- src/mappers.ts
+++ src/mappers.ts
@@ -1,12 +1,22 @@
 import type { Media, MediaFormat, Project } from "@/types";
 
+const isUrl = (url: string): boolean => {
+  try {
+    new URL(url);
+    return true;
+  } catch (e) {
+    return false;
+  }
+
+}
+
 const mediaFormatMapper = (data: any): MediaFormat => {
   return {
     name: data.name,
     hash: data.hash,
     width: data.width,
     height: data.height,
-    url: import.meta.env.VITE_BACKEND_URL + data.url,
+    url: isUrl(data.url) ? data.url : import.meta.env.VITE_BACKEND_URL + data.url,
   }
 }
 
@@ -14,7 +24,7 @@ export const mediaMapper = (data: any): Media => {
   return {
     id: data.id,
     name: data.attributes.name,
-    url: import.meta.env.VITE_BACKEND_URL + data.attributes.url,
+    url: isUrl(data.attributes.url) ? data.attributes.url : import.meta.env.VITE_BACKEND_URL + data.attributes.url,
     alt: data.attributes.alternativeText,
     caption: data.attributes.caption,
     width: data.attributes.width,
